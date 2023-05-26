456c68b9d3421a31d2eace42094b18cdc17b1e2a
jihanurrahman33 <64974221+jihanurrahman33@users.noreply.github.com>
2023-05-26 14:41:43 +0600

    Add files via upload

---
+++ ElectricityBillSystem/src/main/java/com/mycompany/electricitybillsystem/ElectricityBillSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.electricitybillsystem;
+
+/**
+ *
+ * @author nishak
+ */
+public class ElectricityBillSystem {
+
+    public static void main(String[] args) {
+        
+    }
+}
